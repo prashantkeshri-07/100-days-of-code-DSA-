45acfdb84ec58c9e76deed4bafab489c562c2590
Prashant Kumar <prashantkumar002800@gmail.com>
2026-03-21 13:26:56 +0530

    Day49

---
+++ Day49(Q49).cpp
@@ -0,0 +1,99 @@
+//BST Insert
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of BST node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert into BST
+struct Node* insert(struct Node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+// Inorder traversal (Left -> Root -> Right)
+void inorder(struct Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    int n, value, choice;
+
+    printf("===== BST INSERT PROGRAM =====\n");
+
+    // Initial insertion
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value %d: ", i + 1);
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+
+    printf("\nInorder Traversal (Sorted BST): ");
+    inorder(root);
+    printf("\n");
+
+    // Interactive menu
+    do {
+        printf("\n--- MENU ---\n");
+        printf("1. Insert new element\n");
+        printf("2. Display Inorder Traversal\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                printf("Inserted successfully!\n");
+                break;
+
+            case 2:
+                printf("Inorder Traversal: ");
+                inorder(root);
+                printf("\n");
+                break;
+
+            case 3:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+
+    } while (choice != 3);
+
+    return 0;
+}
